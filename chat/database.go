c3daeb89080c2d890d5ccd926496c0e7a34d306c
Chahkiev <chahkiev98@mail.ru>
2019-10-04 00:25:05 +0300

    updated DB schema

--- chat/database.go
+++ chat/database.go
@@ -25,22 +25,47 @@ type Message struct {
 
 func InitDB(db *sql.DB) {
 	qs := []string{
+		`DROP TABLE IF EXISTS messages;`,
+		`DROP TABLE IF EXISTS chat_user;`,
 		`DROP TABLE IF EXISTS users;`,
-
-		`CREATE TABLE users (
-// TODO
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
-
 		`DROP TABLE IF EXISTS chats;`,
 
-		`CREATE TABLE chats (
-// TODO
+		`CREATE TABLE users (
+id int(11) NOT NULL AUTO_INCREMENT,
+username varchar(255) NOT NULL,
+created_at datetime NOT NULL,
+PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
 
-		`DROP TABLE IF EXISTS messages;`,
+		`INSERT INTO users (id, username, created_at) VALUES
+(1,	'user_1',	'2011-12-18 13:17:17'),
+(2,	'user_2',	'2012-12-18 13:17:17');`,
+
+		`CREATE TABLE chats (
+id int(11) NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+create_at datetime NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
+
+		`CREATE TABLE chat_user (
+id int(11) NOT NULL AUTO_INCREMENT,
+chat int(11) NOT NULL,
+user int(11) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (chat) REFERENCES chats(id),
+FOREIGN KEY (user) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
 
 		`CREATE TABLE messages (
-// TODO
+id int(11) NOT NULL AUTO_INCREMENT,
+chat int(11) NOT NULL,
+author int(11) NOT NULL,
+text varchar(255) NOT NULL,
+created_at datetime NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (chat) REFERENCES chats(id),
+FOREIGN KEY (author) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`,
 	}
 
